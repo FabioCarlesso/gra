00790f6b9c93ebd84712a073106e0d7f1e0cf4de
Fabio Carlesso <fabio.carlesso@gmail.com>
2022-10-05 11:17:02 -0300

    Create a simple test to producers interval

--- src/test/java/com/fabiocarlesso/gra/GraApplicationTests.java
+++ src/test/java/com/fabiocarlesso/gra/GraApplicationTests.java
@@ -1,13 +1,42 @@
 package com.fabiocarlesso.gra;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.fabiocarlesso.gra.domain.dto.MovieProducerMinMaxDTO;
 
 @SpringBootTest
 class GraApplicationTests {
 
+	private static final String HOST = "http://localhost:";
+	private static final String MOVIES_PATH = "/v1/movies";
+	private static final String INTERVAL_PATH = "interval";
+
+	private String path;
+
+	@LocalServerPort
+    private int port;
+
+	private TestRestTemplate testRestTemplate;
+
+	@BeforeEach
+    private void init() {
+        this.path = HOST+port+MOVIES_PATH;
+    }
+	
 	@Test
-	void contextLoads() {
+	public void findMinMaxIntervalTest() {
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
+		ResponseEntity<MovieProducerMinMaxDTO> response = testRestTemplate.getForEntity(
+			this.path+INTERVAL_PATH, MovieProducerMinMaxDTO.class);
+		Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+		Assertions.assertNotNull(response.getBody());
 	}
 
 }
