295b9d3ef9b7e297dff78ccb80a6c11a244a46d7
Fabio Carlesso <fabio.carlesso@gmail.com>
2022-10-04 23:40:32 -0300

    REST resources to expose Movie and Producer.

---
+++ src/main/java/com/fabiocarlesso/gra/resource/rest/MovieResource.java
@@ -0,0 +1,44 @@
+package com.fabiocarlesso.gra.resource.rest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fabiocarlesso.gra.domain.Movie;
+import com.fabiocarlesso.gra.domain.dto.MovieDTO;
+import com.fabiocarlesso.gra.service.MovieService;
+
+@RestController
+@RequestMapping(value="/v1/movies")
+public class MovieResource {
+    
+    @Autowired
+    private MovieService service;
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<MovieDTO> find(@PathVariable Integer id){
+        Movie obj = service.find(id);
+        return ResponseEntity.ok().body(new MovieDTO(obj));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<MovieDTO>> findAll(){
+        List<Movie> list = service.findAll();
+        List<MovieDTO> listDto = list.stream().map(MovieDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping(value="/winners")
+    public ResponseEntity<List<MovieDTO>> findWinner(){
+        List<Movie> list = service.findByWinner();
+        List<MovieDTO> listDto = list.stream().map(MovieDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+    
+}
---
+++ src/main/java/com/fabiocarlesso/gra/resource/rest/ProducerResource.java
@@ -0,0 +1,37 @@
+package com.fabiocarlesso.gra.resource.rest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fabiocarlesso.gra.domain.Producer;
+import com.fabiocarlesso.gra.domain.dto.ProducerDTO;
+import com.fabiocarlesso.gra.service.ProducerService;
+
+@RestController
+@RequestMapping(value="/v1/producers")
+public class ProducerResource {
+    
+    @Autowired
+    private ProducerService service;
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<ProducerDTO> find(@PathVariable Integer id){
+        Producer obj = service.find(id);
+        return ResponseEntity.ok().body(new ProducerDTO(obj));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ProducerDTO>> findAll(){
+        List<Producer> list = service.findAll();
+        List<ProducerDTO> listDto = list.stream().map(ProducerDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
+
+}
