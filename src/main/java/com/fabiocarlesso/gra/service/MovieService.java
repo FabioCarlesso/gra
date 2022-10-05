5fab4603e330162bdacbc099f0079f78c483baa1
Fabio Carlesso <fabio.carlesso@gmail.com>
2022-10-05 10:32:32 -0300

    Fix some warnings

--- src/main/java/com/fabiocarlesso/gra/service/MovieService.java
+++ src/main/java/com/fabiocarlesso/gra/service/MovieService.java
@@ -1,9 +1,7 @@
 package com.fabiocarlesso.gra.service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
