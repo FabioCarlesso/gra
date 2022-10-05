17732e05236ca696186cf2ea6763cde81fb66e04
Fabio Carlesso <fabio.carlesso@gmail.com>
2022-10-05 10:29:08 -0300

    Fix some warnings

--- src/main/java/com/fabiocarlesso/gra/domain/dto/MovieProducerMinMaxDTO.java
+++ src/main/java/com/fabiocarlesso/gra/domain/dto/MovieProducerMinMaxDTO.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class MovieProducerMinMaxDTO {
     private List<MovieProducerIntervalDTO> min = new ArrayList<>();
     private List<MovieProducerIntervalDTO> max = new ArrayList<>();
